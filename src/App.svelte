7b98433b81ae22b687ffe65f563247d32f00cdb1
Chris Wong <wc38.wong@gmail.com>
2023-05-29 22:11:43 +0800

    Blocking default click handling by preventDefault

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,8 @@
 <script>
-  let output = '';
-  function checkboxClick(event) {
-    let warn = "preventDefault() won't let you check this!<br>";
-    output += warn;
-  }
+  import PreventDefault from "./lib/PreventDefault.svelte";
 </script>
 
-<p>Please click on the checkbox control.</p>
-
-<form>
-  <label for="id-checkbox">Checkbox:</label>
-  <input type="checkbox"  on:click|preventDefault={checkboxClick}/>
-</form>
-
-<div>{@html output}</div>
-
+<PreventDefault />
 <style>
 
 </style>
---
+++ src/lib/PreventDefault.svelte
@@ -0,0 +1,20 @@
+<script>
+  let output = '';
+  function checkboxClick(event) {
+    let warn = "preventDefault() won't let you check this!<br>";
+    output += warn;
+  }
+</script>
+
+<p>Please click on the checkbox control.</p>
+
+<form>
+  <label for="id-checkbox">Checkbox:</label>
+  <input type="checkbox" id="id-checkbox" on:click|preventDefault={checkboxClick}/>
+</form>
+
+<div>{@html output}</div>
+
+<style>
+
+</style>
