24fb1e16ea8ac342a4f5d376a5021cc2ab5d2003
Chris Wong <wc38.wong@gmail.com>
2023-05-29 11:15:09 +0800

    Set base in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/7-Event-Modifiers-in-Svelte-You-Must-Know/',
   plugins: [svelte()],
 })
